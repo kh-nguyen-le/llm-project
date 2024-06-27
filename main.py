1106678ad7d9c00a1d69c3a11625629f4e9a89bd
John Le <kh.nguyen.le@gmail.com>
2024-06-27 00:41:07 -0400

    spinner

--- main.py
+++ main.py
@@ -12,21 +12,23 @@ sql_database = SQLDatabase(engine, include_tables=[database.TABLE_NAME])
 
 query_engine = NLSQLTableQueryEngine(sql_database, llm, tables=[database.TABLE_NAME], streaming=True)
 
-stream = []
-
 if "messages" not in st.session_state:
-  st.session_state.messages = []
+  st.session_state.messages = [
+      {"role": "assistant", "content": "Ask a Question about the YuGiOh card game!"}
+  ]
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("Ask a Question about the YuGiOh card game: "):
+if prompt := st.chat_input("Question: "):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        stream.append(query_engine.query(prompt).response_gen)
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.spinner("Thinking..."):
+            with st.chat_message("assistant"):
+                stream = query_engine.query(prompt)
+                response = st.write(stream.response_gen)
+            st.session_state.messages.append({"role": "assistant", "content": response})

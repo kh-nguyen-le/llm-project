9e49d544ed1b2bb92fd52f67ddf9ea1199145fe4
John Le <kh.nguyen.le@gmail.com>
2024-07-04 21:30:55 -0400

    added chat history

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core.retrievers import NLSQLRetriever
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.core.agent import ReActAgent
+from llama_index.core.base.llms.types import ChatMessage, MessageRole
+from llama_index.core.storage.chat_store import SimpleChatStore
+from llama_index.core.memory import ChatMemoryBuffer
 import streamlit as st
 
 llm = Ollama(model="llama3", request_timeout=300.0, streaming=True)
@@ -35,13 +38,6 @@ You are an expert on the YuGiOh Card game.
 You will answer questions about cards used in the game from a technical perspective.
 """
 
-agent = ReActAgent.from_tools(
-    query_engine_tools,
-    llm=llm,
-    verbose=True,
-    max_iterations=100,
-    context=context
-)
 
 def stream_wrapper(gen):
     for token in gen:
@@ -49,20 +45,39 @@ def stream_wrapper(gen):
 
 if "messages" not in st.session_state:
   st.session_state.messages = [
-      {"role": "assistant", "content": "Ask a Question about the YuGiOh card game!"}
-  ]
-
+      ChatMessage.from_str(content="Ask a Question about the YuGiOh card game!",
+                           role=MessageRole.ASSISTANT)]
+  
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    with st.chat_message(message.role):
+        st.markdown(message.content)
+chat_store = SimpleChatStore()
+
+chat_memory = ChatMemoryBuffer.from_defaults(
+    chat_history=st.session_state.messages,
+    llm=llm,
+    chat_store=chat_store,
+    chat_store_key="user"
+    )
+
+agent = ReActAgent.from_tools(
+    query_engine_tools,
+    llm=llm,
+    verbose=True,
+    max_iterations=100,
+    context=context,
+    chat_history=st.session_state.messages,
+    memory=chat_memory
+)
 
 if prompt := st.chat_input("Question: "):
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append(ChatMessage.from_str(role=MessageRole.USER, content=prompt))
     with st.chat_message("user"):
         st.markdown(prompt)
     
-    if st.session_state.messages[-1]["role"] != "assistant":
+    if st.session_state.messages[-1].role == MessageRole.USER:
         with st.spinner("Thinking..."):
             with st.chat_message("assistant"):
-                response = st.write_stream(stream_wrapper(agent.stream_chat(prompt).response_gen))
-            st.session_state.messages.append({"role": "assistant", "content": response})
+                response = st.write_stream(stream_wrapper(agent.stream_chat(message=prompt, chat_history=st.session_state.messages).response_gen))
+
+            st.session_state.messages.append(ChatMessage.from_str(role=MessageRole.ASSISTANT, content=response))
